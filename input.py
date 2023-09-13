09da0c9231f79753f34cce904a9036caad54cd0f
Labanya Saha <100686917+Labanya23@users.noreply.github.com>
2023-09-13 12:18:37 +0600

    Add files via upload

---
+++ input.py
@@ -0,0 +1,25 @@
+#print('Now i need money')
+#input()
+#input('Give me some money: ')
+
+#money = input('Give me some money: ')
+#print("here is your money",money)
+
+first_money=input('Kodom ali,dosto kicu taka dey: ')
+second_money=input('peyara begum,dosto kicu taka dey: ')
+print(type(first_money))
+#by default the input from user will be string type:
+print('money i got from kodom',first_money,'and from peyara',second_money)
+first_money_int=int(first_money)
+second_money_int=int(second_money)
+#print(type(first_money_int))
+#total=first_money+second_money
+total= first_money_int+second_money_int
+print('total money I got:', total)
+
+"""
+Google search
+1.convert number to string:str
+2.convert decimal number:float
+list,tuple,hex,oct
+"""
---
+++ variable.py
@@ -0,0 +1,22 @@
+age = 45
+interest_rate=2.5
+name="Jume jo pathan"
+district='B baria'
+is_single=True
+is_sleeping=False
+#crrl +/: single line of comment
+# altr + shift +A : doc string or multi line coment
+"""
+we are exploring variable
+"""
+print(age*interest_rate)
+print(district)
+
+print(type(age))
+#print(type(interest_rate))command line
+print(type(district))
+print(type(is_single))
+
+print('KODOM ALI'+ '' + 'Kacha badam')
+text = f"Kodom al {age} living in {district}.interesting with rate {interest_rate}%"
+print(text)
