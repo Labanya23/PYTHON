6dc48a33fad700a70626e459c8fbe552bc1812c7
Labanya Saha <100686917+Labanya23@users.noreply.github.com>
2023-09-15 10:13:26 +0600

    Add files via upload

---
+++ op.py
@@ -0,0 +1,18 @@
+num1 = 45
+num2 = 5
+print(num1 + num2)
+print(num1 / num2)
+print(num1*num2)
+print(num1-num2)
+print(106/3)
+print(106%3) #vag Sas
+print(106//3)#puro vagfol
+
+# >,<,>=,<=,==,!==
+# ++ -- agulo use kora jai na
+
+#power use kora easy python ai
+power=num1**num2
+print(power)
+
+#python comparison
