fa214f5d1f9949ba0d9822f7fe9111752141c886
Labanya Saha <100686917+Labanya23@users.noreply.github.com>
2023-09-15 11:52:38 +0600

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,40 @@
+# agulo acha : +=,-+,*=,/+
+#in ,not,not in,is,is not
+"""
+a=7
+a = a+1
+a+=5
+"""
+#a = 7
+a=4
+boss=False
+if a > 5:
+ print('5 er bashi')
+ print('koto bahi ke jana')
+elif a>3:
+ print('olpo boro')
+elif(a==2):
+ 
+ print('dui er soman soman')
+  
+else:
+ print('choto number')
+
+if boss is True:
+ print('I will be engineer')
+else:
+ print('I will be developer')
+coin='head'
+ #nested condition
+if bool == True:
+ print('i am joss')
+ if coin == 'tail':
+    print('codding')
+ else:
+    print('developer')
+    if 5>2 or boss!='True':
+      print('do something')
+      if 8%2 == 0:
+        print('eve 8 is even')
+else:
+  print('i will be engineer') 
