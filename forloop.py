369b989a8b2710793962cb10b1f9c2589168fb99
Labanya Saha <100686917+Labanya23@users.noreply.github.com>
2023-09-15 12:46:06 +0600

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,27 @@
+#nums
+#for(int i=0;i<5;i++){
+#int num=nums[i]  C tai use hoi
+#}
+"""
+numbers = [5,10,15,20,25]
+sum=0
+for num in numbers:
+    print(num)
+    sum=sum+num
+    if sum>20:
+        print('bigger values')
+print(sum)
+"""
+#text='Labanya saha'
+#for char in text:
+    #print(char)
+
+#nums=[1,2,3,4,5,6,7,8,9,10]
+#range(1,10)
+
+#print(range(1,10))
+
+for i in range(1,10,2):
+    print(i)
+
+    #first tar tai bolba kotothka suru;second tai bolba sas koi;third tai bolba step kotogulo I mean koto ghor por por
