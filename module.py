65f994725d776ac67d445065bd8b02e5c6f1830f
Labanya Saha <100686917+Labanya23@users.noreply.github.com>
2023-09-17 12:29:19 +0600

    Add files via upload

---
+++ module.py
@@ -0,0 +1,12 @@
+#jdi ono file thka korta chai
+from function import double_it
+from kamultiole import full_name as name
+
+from deafult_args_kargs import *
+f_name=name('labanya','saha')
+print(f_name)
+#result=double_it(45)
+#print(result)
+
+
+#sum(45,98)
